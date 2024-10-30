6c966d95d1d6f09c8cd13e8e0c68ab4c1d371155
刘志伟 <2055572879@qq.com>
2024-10-30 20:22:43 +0800

    Add react-resizable dependency and implement resizable sidebar in layout

--- packages/core/src/layout/index.tsx
+++ packages/core/src/layout/index.tsx
@@ -10,6 +10,8 @@ import { Button, Dropdown, Flex, Layout, Menu, MenuProps, theme } from 'antd'
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Outlet, useNavigate } from 'react-router-dom'
+import { ResizableBox } from 'react-resizable'
+import 'react-resizable/css/styles.css'
 
 const { Header, Sider, Content } = Layout
 
@@ -39,69 +41,64 @@ const DeftLayout: React.FC = () => {
     },
   ]
 
+  const [width, setWidth] = useState(300) // 初始宽度
+
+  const handleResize = (e, { size }) => {
+    setWidth(size.width)
+  }
+
   return (
     <Layout>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth={0}
-        trigger={null}
-        collapsible
-        collapsed={collapsed}
-        onBreakpoint={(broken) => {
-          setCollapsed(broken)
-        }}
+      {/* change to npm install react-split-pane */}
+      <ResizableBox
+        width={width}
+        height={0}
+        onResize={handleResize}
+        resizeHandles={['e']}
       >
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-              onClick: () => navigate('/'),
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-              onClick: () => navigate('/demo'),
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
-        />
-      </Sider>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth={75}
+          trigger={null}
+          collapsible
+          collapsed={collapsed}
+          onBreakpoint={(broken) => {
+            setCollapsed(broken)
+          }}
+        >
+          <div className="demo-logo-vertical" />
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            items={[
+              {
+                key: '1',
+                icon: <UserOutlined />,
+                label: 'nav 1',
+                onClick: () => navigate('/'),
+              },
+              {
+                key: '2',
+                icon: <VideoCameraOutlined />,
+                label: 'nav 2',
+                onClick: () => navigate('/demo'),
+              },
+              {
+                key: '3',
+                icon: <UploadOutlined />,
+                label: 'nav 3',
+              },
+            ]}
+          />
+        </Sider>
+      </ResizableBox>
       <Layout>
-        <Header style={{ padding: '0 24px 0 0', background: colorBgContainer }}>
-          <Flex justify="space-between" align="center">
-            <Button
-              type="text"
-              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-              onClick={() => setCollapsed(!collapsed)}
-              style={{
-                fontSize: '16px',
-                width: 64,
-                height: 64,
-              }}
-            />
-
-            <Dropdown menu={{ items }}>
-              <div onClick={(e) => e.preventDefault()}>
-                {t('layout.language')} <DownOutlined />
-              </div>
-            </Dropdown>
-          </Flex>
-        </Header>
+        {/* <Sider>Sider2</Sider> */}
         <Content
           style={{
-            margin: '24px 16px',
-            padding: 24,
+            overflow: 'hidden',
+            height: '100vh',
             minHeight: 280,
             background: colorBgContainer,
             borderRadius: borderRadiusLG,
